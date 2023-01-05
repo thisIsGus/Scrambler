03c0ec96919d567c1cc68d791d6e330a079da450
thisIsGus <amartinez2024@francisparker.org>
2023-01-05 12:16:00 -0800

    finished recursion search, need the scrambler algorithm

--- GetAllFiles.java
+++ GetAllFiles.java
@@ -1,33 +1,70 @@
 import java.io.File;  // Import the File class
-import java.io.IOException;  // Import the IOException class to handle errors
+//import java.io.IOException;  // Import the IOException class to handle errors
 import java.util.ArrayList; 
+import java.io.FileWriter;
 
 
 
 public class GetAllFiles {
 
-    public ArrayList<File> Files = new ArrayList<File>();
-    public static void main( String[] args ) {
-        System.out.println("sgresd");
+    public static ArrayList< File > files = new ArrayList<File>();
+    public static File placeholder1, placeholder2;
+
+    public static void main( String[] args ) 
+    {
+        File thingy = new File( ".");
+        getFilesInDir( thingy );
+
+        // for ( int index = 0; index < files.size(); index++ ) {
+        //     System.out.println( files.get(index).getName());
+        // }
+        
+        for (int index = 0; index < files.size()-1; index++) {
+            
+        }
     }
 
-    public static void getFilesInDir(File dir) {
+    public static void getFilesInDir( File dir ) {
+        //ArrayList< File > fileStorage = new ArrayList< File >();
         try {
-            File here = new File(".");
+            File here = new File( dir.getAbsolutePath() );
             File[] contents = here.listFiles();
 
-            for(File f : contents) {
-                if(f.isDirectory()) {
-                    getFilesInDir(f);
+            for( File f : contents ) {
+                if(f.isFile() && !f.isHidden()) {
+                    files.add( f );
                 }
-                else {
-                    
+                else if (f.isDirectory() && !f.isHidden()){
+                    getFilesInDir( f );
                 }
-                System.out.println(f.getName());
+                //System.out.println(f.getName());
             }
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
+        } catch ( Exception e ) {
+            System.out.println( "An error occurred." );
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<File> getFiles() {
+        return files;
+    }
+
+    public static void setFiles(ArrayList<File> files) {
+        GetAllFiles.files = files;
+    }
+
+
+    public static void writeToFile(String content, String fileLocation)
+    {
+           File file = new File(fileLocation);
+           FileWriter writer;
+           try {
+               writer = new FileWriter(file);
+               writer.write(content);
+               writer.close();
+           }
+           catch (IOException e) {
+               System.out.println("ABCDEFG");
+           }
     }
 }
