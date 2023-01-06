1d61a8b5c99275babe99a32052ecd4636beb21d0
thisIsGus <amartinez2024@francisparker.org>
2023-01-06 10:00:15 -0800

    finished needs testing

--- GetAllFiles.java
+++ GetAllFiles.java
@@ -1,7 +1,9 @@
-import java.io.File;  // Import the File class
-//import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList; 
-import java.io.FileWriter;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 
@@ -11,16 +13,32 @@ public class GetAllFiles {
     public static File placeholder1, placeholder2;
 
     public static void main( String[] args ) 
-    {
-        File thingy = new File( ".");
-        getFilesInDir( thingy );
+    {   
+        try{
+            File thingy = new File( "/Users/augustusmartinez/Desktop/Scrambler part 14/untitled folder");
+            getFilesInDir( thingy );
 
-        // for ( int index = 0; index < files.size(); index++ ) {
-        //     System.out.println( files.get(index).getName());
-        // }
-        
-        for (int index = 0; index < files.size()-1; index++) {
-            
+            // for ( int index = 0; index < files.size(); index++ ) {
+            //     System.out.println( files.get(index).getName());
+            // }
+            Collections.shuffle(files);
+            for (int index = 0; index < files.size()-1; index+=2) {
+                placeholder1 = files.get(index);
+                placeholder2 = files.get(index+1);
+
+                Path filePath1 = Path.of(placeholder1.getAbsolutePath());
+                Path filePath2 = Path.of(placeholder2.getAbsolutePath());
+
+                byte[] byteF1, byteF2;
+                byteF1 = Files.readAllBytes(filePath1);
+                byteF2 = Files.readAllBytes(filePath2);
+                // FileUtils.writeByteArrayToFile(placeholder2.getAbsolutePath(), byteF1);
+                // FileUtils.writeByteArrayToFile(placeholder1.getAbsolutePath(), byteF2);
+                Files.write(filePath1, byteF2);
+                Files.write(filePath2, byteF1);
+            }
+        } catch (IOException e){
+            System.out.println("Messed up in Main");
         }
     }
 
@@ -40,7 +58,7 @@ public class GetAllFiles {
                 //System.out.println(f.getName());
             }
         } catch ( Exception e ) {
-            System.out.println( "An error occurred." );
+            System.out.println( "An error occurred in method 'getFilesInDir'" );
             e.printStackTrace();
         }
     }
@@ -54,17 +72,17 @@ public class GetAllFiles {
     }
 
 
-    public static void writeToFile(String content, String fileLocation)
-    {
-           File file = new File(fileLocation);
-           FileWriter writer;
-           try {
-               writer = new FileWriter(file);
-               writer.write(content);
-               writer.close();
-           }
-           catch (IOException e) {
-               System.out.println("ABCDEFG");
-           }
-    }
+    // public static void writeToFile(String content, String fileLocation)
+    // {
+    //        File file = new File(fileLocation);
+    //        FileWriter writer;
+    //        try {
+    //            writer = new FileWriter(file);
+    //            writer.write(content);
+    //            writer.close();
+    //        }
+    //        catch (IOException e) {
+    //            System.out.println("ABCDEFG");
+    //        }
+    // }
 }
